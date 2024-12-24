7a3e689cd9b9923cd972a16b4f31574b80130b16
AgusDana <nyomanyuda9@gmail.com>
2024-12-24 10:18:02 +0800

    Login register

--- pkl/app/Models/User.php
+++ pkl/app/Models/User.php
@@ -15,18 +15,19 @@ class User extends Authenticatable
     /**
      * The attributes that are mass assignable.
      *
-     * @var list<string>
+     * @var array
      */
     protected $fillable = [
         'name',
         'email',
         'password',
+        'google_id'
     ];
 
     /**
      * The attributes that should be hidden for serialization.
      *
-     * @var list<string>
+     * @var array
      */
     protected $hidden = [
         'password',
@@ -36,7 +37,7 @@ class User extends Authenticatable
     /**
      * Get the attributes that should be cast.
      *
-     * @return array<string, string>
+     * @return array
      */
     protected function casts(): array
     {
--- pkl/routes/web.php
+++ pkl/routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\GoogleController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// authentikasi google
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::controller(GoogleController::class)->group(function() {
+    Route::get('auth/google', 'redirectToGoogle')->name('auth.google');
+    Route::get('auth/google/callback', 'handleGoogleCallback');
 });
